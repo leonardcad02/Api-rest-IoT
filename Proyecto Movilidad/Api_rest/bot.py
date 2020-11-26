35cb1ebafd071e9c2bb11acc9ef78b36642bf7ac
leonardcad02 <leonardcad02@gmail.com>
2020-11-26 09:53:07 -0500

    bot.py

--- Proyecto Movilidad/Api_rest/bot.py
+++ Proyecto Movilidad/Api_rest/bot.py
@@ -1,28 +1,15 @@
+#!/usr/bin/env python3
 # Importamos paquetes que necesitamos
 import os
-import tweepy
 import requests
 import time
 import datetime
 
-# Obtener las llaves de la app de Twitter desde variables de entorno por seguridad
-consumer_key = os.environ.get('CONSUMER_KEY')
-consumer_secret = os.environ.get('CONSUMER_SECRET')
-access_token = os.environ.get('ACCESS_TOKEN')
-access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
-# Autenticar utilizando estas llaves
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-
-# Configura el access token
-auth.set_access_token(access_token, access_token_secret)
-
-# Obtiene objeto de API de consulta de twitter
-api = tweepy.API(auth)
 
 # Consulta los datos del sensor en la IP del Wemos
-response = requests.get('http://192.168.0.24:80')
-response2 = requests.get('http://192.168.0.27:80')
+response = requests.get('http://192.168.1.80:80')
+#response2 = requests.get('http://192.168.0.27:80')
 
 # Setup del timestamp 
 t = time.time()
@@ -30,20 +17,17 @@ timegood = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
 
 # Convierte la respuesta del servidor de Wemos en un diccionario de Python
 sensor_data = response.json()
-sensor_data2 = response2.json()
 
 temperature = sensor_data['variables']['temperature']
 humidity = sensor_data['variables']['humidity']
 pollution = sensor_data['variables']['contaminacion']
 
-temperature2 = sensor_data2['variables']['temperature']
-humidity2 = sensor_data2['variables']['humidity']
-pollution2 = sensor_data2['variables']['contaminacion']
+
 
 # Hace el promedio de las mediciones
-avtemp = ((temperature+temperature2/2)) 
-avhumi = ((humidity+humidity2)/2)
-avpoll = ((pollution+pollution2)/2)
+#avtemp = ((temperature+temperature2/2)) 
+#avhumi = ((humidity+humidity2)/2)
+#avpoll = ((pollution+pollution2)/2)
 
 # Envia el tweet en mi cuenta
-api.update_status('Soy un bot creado por Cristian \n La temperatura es: {0} ℃ \n La humedad es: {1} %% RH \n La concentración es: {2} %% PPM \n {3}' .format(avtemp, avhumi, avpoll, timegood))
+print('Soy un bot creado por Cristian \n La temperatura es: {0} ℃ \n La humedad es: {1} %% RH \n La concentración es: {2} %% PPM \n {3}' .format(temperature, humidity, pollution, timegood))
